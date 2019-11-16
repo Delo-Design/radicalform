cdc713c44da1f63f2931767fb74d2c12d136e835
Dmitrii <tsymbal.mitia@gmail.com>
2019-11-16 21:52:56 +0300

    Update script.js
    Обертка в функцию и добавление в глобальную область видимость

--- media/js/script.js
+++ media/js/script.js
@@ -8,167 +8,79 @@ function ready(fn) {
 
 
 ready(function () {
-    function closest(el, selector) {
-        var matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+    RadicalForm.RadicalFormClass = new RadicalFormClass;
+    RadicalForm.RadicalFormClass.init();
+});
 
-        while (el) {
-            if (matchesSelector.call(el, selector)) {
-                return el;
-            } else {
-                el = el.parentElement;
-            }
+
+RadicalFormClass = function () {
+
+    var selfClass = this;
+
+    /**
+     * get uniq id for upload a file.
+     * @type {number}
+     */
+    this.uniq = (new Date).getTime() + Math.floor(Math.random() * 100);
+
+    /**
+     *
+     * @type {array}
+     */
+    this.danger_сlasses = RadicalForm.DangerClass.split(" ");
+
+
+    /**
+     * Init for DOM element
+     * @param container
+     */
+    this.init = function(container) {
+
+        if(container === null || container === undefined) {
+            container = document.querySelector('body')
         }
-        return null;
-    }
 
-    var uniq = (new Date).getTime() + Math.floor(Math.random() * 100); // get uniq id for upload a file.
-
-
-    document.querySelector(".rf-button-send");
-
-    [].forEach.call(document.querySelectorAll('.rf-button-send'), function (el) {
-        el.insertAdjacentHTML('afterend', '<input type="hidden" name="uniq" value="' + uniq + '" />');
-        el.insertAdjacentHTML('afterend', RadicalForm.Token);
-        el.insertAdjacentHTML('afterend', '<input type="hidden" name="url" value="' + window.location.href + '" />');
-        el.insertAdjacentHTML('afterend', '<input type="hidden" name="resolution" value="' + screen.width + 'x' + screen.height + '" />');
-        el.insertAdjacentHTML('afterend', '<input type="hidden" name="pagetitle" value="' + document.title.replace(/&/g, "&amp;")
-            .replace(/</g, "&lt;")
-            .replace(/>/g, "&gt;")
-            .replace(/"/g, "&quot;")
-            .replace(/'/g, "&#039;") + '" />');
-        el.insertAdjacentHTML('afterend', '<input type="hidden" name="reffer" value="' + document.referrer + '" />');
-    });
-
-
-    if (document.querySelectorAll(".rf-filenames-list").length !== document.querySelectorAll("form .rf-filenames-list").length) {
-        alert('ERROR!\r\nThere is \r\n.rf-filenames-list\r\n outside of form!\r\n Please move .rf-filenames-list inside the form. ');
-    }
-
-    RadicalForm.DangerClasses = RadicalForm.DangerClass.split(" ");
-
-    var on = function (el, selector, event, cb) {
-        el.addEventListener(event, function (e) {
-            for (var target = e.target; target && target != this; target = target.parentNode) {
-                var matchesSelector = target.matches || target.webkitMatchesSelector || target.mozMatchesSelector || target.msMatchesSelector;
-                if (matchesSelector.call(target, selector)) {
-                    cb.call(target, e);
-                    break;
-                }
-            }
-        }, false);
-    };
-
-    on(document.querySelector('body'), "form ." + RadicalForm.DangerClasses, 'keypress', function (target, e) {
-        RadicalForm.DangerClasses.forEach(function (item) {
-            target.target.classList.remove(item);
+        [].forEach.call(container.querySelectorAll('.rf-button-send'), function (el) {
+            el.insertAdjacentHTML('afterend', '<input type="hidden" name="uniq" value="' + selfClass.uniq + '" />');
+            el.insertAdjacentHTML('afterend', RadicalForm.Token);
+            el.insertAdjacentHTML('afterend', '<input type="hidden" name="url" value="' + window.location.href + '" />');
+            el.insertAdjacentHTML('afterend', '<input type="hidden" name="resolution" value="' + screen.width + 'x' + screen.height + '" />');
+            el.insertAdjacentHTML('afterend', '<input type="hidden" name="pagetitle" value="' + document.title.replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/"/g, "&quot;")
+                .replace(/'/g, "&#039;") + '" />');
+            el.insertAdjacentHTML('afterend', '<input type="hidden" name="reffer" value="' + document.referrer + '" />');
         });
-    });
 
-    [].forEach.call(document.querySelectorAll("input[type='file'].rf-upload-button"), function (el) {
-        el.addEventListener('change', FileSend);
-    });
+        this.on(container, "form ." + selfClass.danger_сlasses.join('.'), 'keypress', function (target, e) {
+            selfClass.danger_сlasses.forEach(function (item) {
+                target.target.classList.remove(item);
+            });
+        });
 
+        if (container.querySelectorAll(".rf-filenames-list").length !== container.querySelectorAll("form .rf-filenames-list").length) {
+            alert('ERROR!\r\nThere is \r\n.rf-filenames-list\r\n outside of form!\r\n Please move .rf-filenames-list inside the form. ');
+        }
 
-    // file upload
-    function FileSend(e) {
-            if (!this.getAttribute("name")) {
-                alert("RadicalForm: There is no 'name' attribute for rf-upload-button!\r\nFile can't uploaded. Please, add name attribute for file input tag.");
-                return;
-            }
+        [].forEach.call(container.querySelectorAll('form .rf-button-send'), function (el) {
+            el.addEventListener('click', selfClass.formSend);
+        });
 
-            var textForUploadButton = this.parentNode.querySelector('.rf-upload-button-text'),
-                previousTextForUploadButton = textForUploadButton ? textForUploadButton.innerHTML : "",
-                formData = new FormData(),
-                form = closest(this, 'form'),
-                rf_filenames_list = form.querySelector('.rf-filenames-list');
-
-            formData.append(this.name, this.files[0]);
-
-            if (this.files[0].size < RadicalForm.MaxSize) {
-                formData.append("uniq", uniq);
-
-                if(textForUploadButton) {
-                    textForUploadButton.innerHTML = RadicalForm.waitingForUpload;
-                    textForUploadButton.disabled = true;
-                }
-
-
-
-                var request = new XMLHttpRequest(),
-                    requestUrl = RadicalForm.Base + '/index.php?option=com_ajax&plugin=radicalform&format=json&group=system&file=1&size=' + this.files[0].size;
-
-                request.open('POST', requestUrl);
-                request.send(formData);
-                request.onreadystatechange = function () {
-                    if (this.readyState === 4 && this.status === 200) {
-                        if(textForUploadButton) {
-                            textForUploadButton.disabled = false;
-                            textForUploadButton.innerHTML = previousTextForUploadButton;
-                        }
-                        var response = false;
-                        try {
-                            response = JSON.parse(this.response);
-                        } catch (e) {
-                            response = false;
-                            console.error(request.status + ' ' + e.message);
-                            return;
-                        }
-                        if (response.success) {
-                            var el=rf_filenames_list.querySelector("." + RadicalForm.ErrorFile);
-
-                            if (el) {
-                                el.parentNode.removeChild(el);
-                            }
-                            if ("error" in response.data[0]) {
-                                rf_filenames_list.insertAdjacentHTML('beforeend', "<div class='" + RadicalForm.ErrorFile + "'>" + response.data[0].error + "</div>");
-                            } else {
-                                if (rf_filenames_list.textContent.trim() === "") {
-                                    rf_filenames_list.insertAdjacentHTML('beforeend', "<div>" + RadicalForm.thisFilesWillBeSend + "</div>");
-                                    form.insertAdjacentHTML('beforeend', '<input type="hidden" name="needToSendFiles" value="1" />');
-                                }
-                                rf_filenames_list.insertAdjacentHTML('beforeend', "<div>" + response.data[0].name + "</div>");
-                            }
-
-                        } else {
-                            rf_filenames_list.insertAdjacentHTML('beforeend', "<div>" + response.message + "</div>");
-                        }
-                    } else if (this.readyState === 4 && this.status !== 200) {
-                        if(textForUploadButton) {
-                            textForUploadButton.disabled = false;
-                            textForUploadButton.innerHTML = previousTextForUploadButton;
-                        }
-
-                        try {
-                            rfCall_2((request.status + ' ' + request.message), buttonPressed);
-                        } catch (e) {
-                            console.error('Radical Form JS Code: ', e);
-                        }
-                        console.error(request.status + ' ' + request.message);
-                    }
-                };
-
-            } else {
-                var el=rf_filenames_list.querySelector("." + RadicalForm.ErrorFile);
-
-                if (el) {
-                    el.parentNode.removeChild(el);
-                }
-
-                rf_filenames_list.insertAdjacentHTML('beforeend',"<div class='" + RadicalForm.ErrorFile + "'>" + RadicalForm.ErrorMax + "</div>"); // size is more than limit
-            }
+        [].forEach.call(container.querySelectorAll("input[type='file'].rf-upload-button"), function (el) {
+            el.addEventListener('change', selfClass.fileSend);
+        });
 
     }
 
-    [].forEach.call(document.querySelectorAll('form .rf-button-send'), function (el) {
-        el.addEventListener('click', FormSend);
-    });
-
-    function FormSend(e) {
+    /**
+     * Send form
+     * @param e
+     */
+    this.formSend = function(e) {
         var needReturn = false,
             field,
-            form = closest(this,'form');
-
-
+            form = selfClass.closest(this, 'form');
 
         var numberOfInputsWithNames=form.querySelectorAll("input[name]").length - form.querySelectorAll('input[type="file"]').length;
         if (numberOfInputsWithNames < 7) {
@@ -177,7 +89,7 @@ ready(function () {
         }
         RadicalForm.FormFields = [];
         [].forEach.call(form.querySelectorAll("[name]"), function (el) {
-            RadicalForm.DangerClasses.forEach(function (item) {
+            selfClass.danger_сlasses.forEach(function (item) {
                 el.classList.remove(item);
             });
         });
@@ -195,7 +107,7 @@ ready(function () {
 
         setTimeout(function () {
             for (var i = 0; i < RadicalForm.FormFields.length; i++) {
-                RadicalForm.DangerClasses.forEach(function (item) {
+                selfClass.danger_сlasses.forEach(function (item) {
                     RadicalForm.FormFields[i].classList.add(item);
                 })
             }
@@ -262,7 +174,7 @@ ready(function () {
             }
 
             var  request = new XMLHttpRequest(),
-                 requestUrl = RadicalForm.Base + "/index.php?option=com_ajax&plugin=radicalform&group=system&format=json";
+                requestUrl = RadicalForm.Base + "/index.php?option=com_ajax&plugin=radicalform&group=system&format=json";
             request.open('POST', requestUrl);
             request.send(AjaxFormData);
             request.onreadystatechange = function () {
@@ -352,7 +264,7 @@ ready(function () {
                         try {
                             rfCall_2((response.message),buttonPressed);
                         } catch (e) {
-                        console.error('Radical Form JS Code: ', e);
+                            console.error('Radical Form JS Code: ', e);
                         }
                     }
                 } else if (this.readyState === 4 && this.status !== 200) {
@@ -369,4 +281,120 @@ ready(function () {
         e.preventDefault();
     }
 
-});
+    /**
+     * Send file to server for save
+     * @param e
+     */
+    this.fileSend = function(e) {
+        if (!this.getAttribute("name")) {
+            alert("RadicalForm: There is no 'name' attribute for rf-upload-button!\r\nFile can't uploaded. Please, add name attribute for file input tag.");
+            return;
+        }
+
+        var textForUploadButton = this.parentNode.querySelector('.rf-upload-button-text'),
+            previousTextForUploadButton = textForUploadButton ? textForUploadButton.innerHTML : "",
+            formData = new FormData(),
+            form = selfClass.closest(this, 'form'),
+            rf_filenames_list = form.querySelector('.rf-filenames-list');
+
+        formData.append(this.name, this.files[0]);
+
+        if (this.files[0].size < RadicalForm.MaxSize) {
+            formData.append("uniq", selfClass.uniq);
+
+            if(textForUploadButton) {
+                textForUploadButton.innerHTML = RadicalForm.waitingForUpload;
+                textForUploadButton.disabled = true;
+            }
+
+            var request = new XMLHttpRequest(),
+                requestUrl = RadicalForm.Base + '/index.php?option=com_ajax&plugin=radicalform&format=json&group=system&file=1&size=' + this.files[0].size;
+
+            request.open('POST', requestUrl);
+            request.send(formData);
+            request.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    if(textForUploadButton) {
+                        textForUploadButton.disabled = false;
+                        textForUploadButton.innerHTML = previousTextForUploadButton;
+                    }
+                    var response = false;
+                    try {
+                        response = JSON.parse(this.response);
+                    } catch (e) {
+                        response = false;
+                        console.error(request.status + ' ' + e.message);
+                        return;
+                    }
+                    if (response.success) {
+                        var el=rf_filenames_list.querySelector("." + RadicalForm.ErrorFile);
+
+                        if (el) {
+                            el.parentNode.removeChild(el);
+                        }
+                        if ("error" in response.data[0]) {
+                            rf_filenames_list.insertAdjacentHTML('beforeend', "<div class='" + RadicalForm.ErrorFile + "'>" + response.data[0].error + "</div>");
+                        } else {
+                            if (rf_filenames_list.textContent.trim() === "") {
+                                rf_filenames_list.insertAdjacentHTML('beforeend', "<div>" + RadicalForm.thisFilesWillBeSend + "</div>");
+                                form.insertAdjacentHTML('beforeend', '<input type="hidden" name="needToSendFiles" value="1" />');
+                            }
+                            rf_filenames_list.insertAdjacentHTML('beforeend', "<div>" + response.data[0].name + "</div>");
+                        }
+
+                    } else {
+                        rf_filenames_list.insertAdjacentHTML('beforeend', "<div>" + response.message + "</div>");
+                    }
+                } else if (this.readyState === 4 && this.status !== 200) {
+                    if(textForUploadButton) {
+                        textForUploadButton.disabled = false;
+                        textForUploadButton.innerHTML = previousTextForUploadButton;
+                    }
+
+                    try {
+                        rfCall_2((request.status + ' ' + request.message), buttonPressed);
+                    } catch (e) {
+                        console.error('Radical Form JS Code: ', e);
+                    }
+                    console.error(request.status + ' ' + request.message);
+                }
+            };
+
+        } else {
+            var el=rf_filenames_list.querySelector("." + RadicalForm.ErrorFile);
+
+            if (el) {
+                el.parentNode.removeChild(el);
+            }
+
+            rf_filenames_list.insertAdjacentHTML('beforeend',"<div class='" + RadicalForm.ErrorFile + "'>" + RadicalForm.ErrorMax + "</div>"); // size is more than limit
+        }
+
+    }
+
+    this.on = function (el, selector, event, cb) {
+        el.addEventListener(event, function (e) {
+            for (var target = e.target; target && target != this; target = target.parentNode) {
+                var matchesSelector = target.matches || target.webkitMatchesSelector || target.mozMatchesSelector || target.msMatchesSelector;
+                if (matchesSelector.call(target, selector)) {
+                    cb.call(target, e);
+                    break;
+                }
+            }
+        }, false);
+    };
+
+    this.closest =  function(el, selector) {
+        var matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+
+        while (el) {
+            if (matchesSelector.call(el, selector)) {
+                return el;
+            } else {
+                el = el.parentElement;
+            }
+        }
+        return null;
+    }
+
+}
